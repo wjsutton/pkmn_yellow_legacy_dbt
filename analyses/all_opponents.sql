f1978d941d39ef9b7dbdbdf642e1117cf627b4e7
Will Sutton <will.sutton+pkmn@theinformationlab.co.uk>
2025-08-24 15:40:14 +0000

    querying output teams

---
+++ analyses/all_opponents.sql
@@ -0,0 +1,9 @@
+SELECT
+player_pokemon,
+player_pkmn_move,
+battle_score
+FROM {{ ref('int_battle_analysis') }}
+WHERE game_stage = 'Badge_3'
+AND trainer = 'Lt._Surge'
+AND battle_score IS NOT NULL
+ORDER BY battle_score DESC
