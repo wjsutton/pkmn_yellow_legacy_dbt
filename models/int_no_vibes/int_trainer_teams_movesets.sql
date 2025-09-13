2b6ce18629a078a4659a645ae16a4866c05e91b2
Will Sutton <wjsutton.dev+pkmn@outlook.com>
2025-09-13 20:30:13 +0000

    created movesets, trainer teams, pre matchups sql

---
+++ models/int_no_vibes/int_trainer_teams_movesets.sql
@@ -0,0 +1,102 @@
+WITH trainers_with_defined_moves AS (
+    SELECT 
+        trainer,
+        'Legendary' as type,
+        pkmn_id,
+        nearest_route,
+        pokemon,
+        game_stage,
+        pkmn_level,
+        move,
+        move_number
+    FROM {{ ref('stg_trainers_legendary') }}
+    
+    UNION ALL
+    
+    SELECT 
+        trainer,
+        'Gym Leader' as type,
+        pkmn_id,
+        nearest_route,
+        pokemon,
+        game_stage,
+        pkmn_level,
+        move,
+        move_number
+    FROM {{ ref('stg_trainers_gym_leaders') }}
+),
+
+trainers_without_defined_moves AS (
+
+    SELECT 
+        T.trainer,
+        'Mandatory' as type,
+        T.pkmn_id,
+        T.nearest_route,
+        T.pokemon,
+        T.game_stage,
+        T.pkmn_level,
+        M.move,
+        ROW_NUMBER() OVER (PARTITION BY T.pkmn_id ORDER BY M.move) AS move_number
+    FROM {{ ref('stg_trainers_mandatory') }} as T
+    INNER JOIN {{ ref('int_pkmn_movesets') }} as M 
+        ON T.pokemon = M.pokemon
+    WHERE M.move_origin = 'level-up'
+        AND T.pkmn_level >= M.pkmn_level
+    QUALIFY ROW_NUMBER() OVER(PARTITION BY T.pkmn_id ORDER BY M.move) <= 4
+),
+
+all_trainers AS (
+
+    SELECT * FROM trainers_with_defined_moves
+    UNION ALL
+    SELECT * FROM trainers_without_defined_moves
+),
+
+trainer_roster AS (
+    SELECT
+        T.trainer,
+        T.type,
+        T.game_stage,
+        R.game_order, 
+        T.pkmn_id,
+        S.pokedex,
+        T.pokemon,
+        T.pkmn_level,
+        T.move
+    FROM all_trainers as T
+    INNER JOIN {{ ref('stg_game_route_order') }} as R ON T.nearest_route = R.map
+    INNER JOIN {{ ref('stg_pkmn_stats') }} as S ON T.pokemon = S.pokemon
+    WHERE LOWER(T.game_stage) NOT LIKE '%_alt%'
+    GROUP BY ALL
+)
+
+SELECT
+    -- Trainer identification
+    tr.trainer,
+    tr.type,
+    tr.game_stage,
+    tr.game_order,
+
+    CASE
+        WHEN tr.trainer LIKE 'Rival_1' THEN NULL
+        WHEN tr.trainer LIKE 'Rival_2' THEN NULL
+        WHEN tr.trainer LIKE 'Rival_3' THEN NULL
+        WHEN tr.trainer LIKE 'Rival_4' THEN NULL
+        WHEN tr.trainer LIKE 'Rival_%_%' 
+        THEN REGEXP_SUBSTR(tr.trainer, '[^_]+$', 1, 1)
+        WHEN tr.trainer LIKE 'Champion_%' 
+        THEN REGEXP_SUBSTR(tr.trainer, '[^_]+$', 1, 1)
+        ELSE NULL
+    END as rival_variant,
+    
+    -- Pokemon details
+    tr.pkmn_id,
+    tr.pokedex,
+    tr.pokemon,
+    tr.pkmn_level,
+    
+    tr.move
+
+FROM trainer_roster as tr
+ORDER BY game_order
